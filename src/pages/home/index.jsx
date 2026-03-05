607ad7a5ad3cb5f972ef94128b1a85d60d2a3e46
washington <washingtongui678@gmail.com>
2026-03-05 17:00:56 -0300

    Criando aplicação react
    Instalando dependencias, vite, organizando pastas e criando repositorio para primeiro commit.

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react' //
+import './style.css'
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      
+
+    </div>
+
+
+  )
+}
+
+export default Home
