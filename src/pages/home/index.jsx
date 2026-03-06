8b03af576f84bd927ed25373fc1d4c5526581fca
Washington <washingtongui678@gmail.com>
2026-03-06 20:08:37 -0300

    Atualizando index
    Criando os inputs e o butão no index

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,32 @@
 import { useState } from 'react' //
 import './style.css'
+import Trash from '../../assets/trash.png'
 
 function Home() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      
+    <div className='container'>
+      <form>
+        <h1>CADASTRO DE USUÁRIOS</h1>
+        <input name='name' type="text" />
+        <input name='idade' type="number" />
+        <input name='email' type="email" />
+        <button type='button'>Cadastrar</button>
+      </form>
 
+      <div>
+        <div>
+          <p>Nome: </p>
+          <p>Idade: </p>
+          <p>Email: </p>
+        </div>
+        <div>
+          <button>
+            <img src={Trash}/>
+          </button>
+        </div>
+      </div>
     </div>
 
 
