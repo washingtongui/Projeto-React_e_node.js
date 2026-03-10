b14fcdf3fe5daa9f96c0d7d44aa2fea8fa4b7c61
washington <washingtongui678@gmail.com>
2026-03-10 17:30:50 -0300

    Atualizando style.css e index.jsx
    Criando a tela de cadastro de usuários e estilizando.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,30 +3,51 @@ import './style.css'
 import Trash from '../../assets/trash.png'
 
 function Home() {
-  const [count, setCount] = useState(0)
+  const users = [{
+    id: '123qwe',
+    name: 'Washington',
+    age: 33,
+    email: 'Washingtongui@gmail.com',
+  }, {
+    id: '123rty',
+    name: 'Lucas',
+    age: 32,
+    email: 'Lucas@gmail.com',
+  }, {
+    id: '134rty',
+    name: 'João',
+    age: 45,
+    email: 'Joao@gmail.com',
+  }
+  ]
 
   return (
     <div className='container'>
       <form>
         <h1>CADASTRO DE USUÁRIOS</h1>
-        <input name='name' type="text" />
-        <input name='idade' type="number" />
-        <input name='email' type="email" />
+        <input placeholder='Nome' name='name' type="text" />
+        <input placeholder='Idade' name='idade' type="number" />
+        <input placeholder='E-mail' name='email' type="email" />
         <button type='button'>Cadastrar</button>
       </form>
 
-      <div>
-        <div>
-          <p>Nome: </p>
-          <p>Idade: </p>
-          <p>Email: </p>
+      {users.map(user => (
+
+        <div key={user.id} className='card'>
+          <div>
+            <p>Nome:  <span>{user.name}</span></p>
+            <p>Idade: <span>{user.age}</span></p>
+            <p>Email: <span>{user.email}</span></p>
+          </div>
+          <div>
+            <button>
+              <img src={Trash} />
+            </button>
+          </div>
         </div>
-        <div>
-          <button>
-            <img src={Trash}/>
-          </button>
-        </div>
-      </div>
+
+      ))}
+
     </div>
 
 
